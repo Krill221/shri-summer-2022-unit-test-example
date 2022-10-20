eb95db7defaa153fb32f2775db379d6dcba73058
Еропкин Кирилл <krill2211@yandex.ru>
2022-10-20 12:49:04 +0300

    -v 0.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-export const testFunction = (a, b) => a + b;
+export const checkInn = (inn) => {
+    if (inn.includes(' ')) return false
+    if (!/\d+$/.test(inn)) return false
+
+    const len = inn.length
+    if (inn.length !== 10 && inn.length !== 12) return false
+
+    const numbers = inn.split('')
+    if (len === 10) {
+        const cont_sum = 2 * numbers[0] + 4 * numbers[1] + 10 * numbers[2] + 3 * numbers[3] + 5 * numbers[4] + 9 * numbers[5] + 4 * numbers[6] + 6 * numbers[7] + 8 * numbers[8]
+        const cont_num = cont_sum % 11 % 10
+        if (parseInt(numbers[9]) !== parseInt(cont_num)) return false
+    }
+    if (len === 12) {
+        const cont_num1 = ((7 * numbers[0] + 2 * numbers[1] + 4 * numbers[2] + 10 * numbers[3] + 3 * numbers[4] + 5 * numbers[5] + 9 * numbers[6] + 4 * numbers[7] + 6 * numbers[8] + 8 * numbers[9]) % 11) % 10
+        const cont_num2 = ((3 * numbers[0] + 7 * numbers[1] + 2 * numbers[2] + 4 * numbers[3] + 10 * numbers[4] + 3 * numbers[5] + 5 * numbers[6] + 9 * numbers[7] + 4 * numbers[8] + 6 * numbers[9] + 8 * numbers[10]) % 11) % 10
+
+        const res1 = parseInt(numbers[10]) !== cont_num1
+        const res2 = parseInt(numbers[11]) !== cont_num2
+
+        if (res1 || res2) return false
+    }
+
+    return true
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,38 @@
-import {testFunction} from '.';
+import { checkInn } from '.';
 
-describe('Тестовая функция, для наглядного примера', () => {
-    it('Складывает два числа вместе', () => {
-        expect(testFunction(2, 3)).toBe(5);
-    });
+describe('Check if inn is correct', () => {
+    it('inn has no spaces', () => {
+        expect(checkInn(' 0000000000')).toBe(false)
+        expect(checkInn('0000 000000')).toBe(false)
+        expect(checkInn('0000000000 ')).toBe(false)
+    })
+    it('inn has length of 10', () => {
+        const inn = '00000000001111'
+        expect(checkInn(inn)).toBe(false)
+    })
+    it('inn has length of 12', () => {
+        const inn = '00000000001111'
+        expect(checkInn(inn)).toBe(false)
+    })
+    it('inn has only digits', () => {
+        const inn = '123456789a'
+        expect(checkInn(inn)).toBe(false)
+    })
+    it('inn with 10 digits wrong control sum', () => {
+        const inn = '1234567890'
+        expect(checkInn(inn)).toBe(false)
+    })
+    it('inn with 10 digits right control sum', () => {
+        const inn = '7704113772'
+        expect(checkInn(inn)).toBe(true)
+    })
+    it('inn with 12 digits wrong control sum', () => {
+        const inn = '123456789012'
+        expect(checkInn(inn)).toBe(false)
+    })
+    it('inn with 12 digits right control sum', () => {
+        const inn = '500100732259'
+        expect(checkInn(inn)).toBe(true)
+    })
 });
+
